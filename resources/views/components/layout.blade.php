b6f9978ee2423f8ff0837f977e75f31ef5f6f206
unknown <akhmadfadhil512@gmail.com>
2025-08-20 08:53:47 +0700

    add table

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,7 @@
 <div>
     <!-- It always seems impossible until it is done. - Nelson Mandela -->
 </div><!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100 dark:bg-gray-900">
+<html lang="en" class="h-full bg-gray-100 dark:bg-gray-500">
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
