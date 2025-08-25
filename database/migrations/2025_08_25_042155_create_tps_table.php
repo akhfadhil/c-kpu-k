6dbbe18ead68b0d67ccaa8fa843f24d77c1b0465
unknown <akhmadfadhil512@gmail.com>
2025-08-25 11:31:51 +0700

    add table migration

---
+++ database/migrations/2025_08_25_042155_create_tps_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_desa')->constrained('desa')->onDelete('cascade');
+            $table->string('kode', 50)->unique(); // kode TPS, misalnya "TPS-001"
+            $table->string('lokasi_deskripsi', 255)->nullable(); // alamat/lokasi TPS
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tps');
+    }
+};
